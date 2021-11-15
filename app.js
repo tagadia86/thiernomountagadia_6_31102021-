f2831c9cf23307fefe15022a3a73278d097e8316
Thierno Dia <tagadia86@gmail.com>
2021-11-15 14:49:13 -0600

    POST and GET are OK on postman

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const bodyParser = require('body-parser'); //pour extraire l'objet json de la demande POST
+const bodyParser = require('body-parser'); //on importe body parser
+const mongoose = require('mongoose');//importer mongoose
+
+const sauceModel = require('./models/sauces');//importation du model créé
+
 const app = express();
-const Sauces = require('./models/sauces');//importer le model mongoose
 
-//partie mongoose
-const mongoose = require('mongoose');
-
-//parametrage de mongoose pour accéder àla base de données
-mongoose.connect('mongodb+srv://thierno_projet_5:ZYiJZiR8WsaR80nY@cluster0.kf6jk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+//connexion à la base de données mongoDB
+mongoose.connect('mongodb+srv://thierno_projet_5:passwordprojet6@cluster0.kf6jk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -21,11 +21,41 @@ app.use((req, res, next) => {
     next();
   });
 
-//inclusion de body parser avant toutes les requetes
-  app.use(bodyParser.json()); //utilisation de body parser pour toutes les routes pour transformer le corps de la requete en objet js utilisable
-//création d'une requete POST
-app.use((req, res) => {
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
-});
+  app.use(bodyParser.json()); //app.use parce que ce sera pour toutes les routes de l'appli ==> json est une methode de body parser qui va transformer le corps de la requete en objet js utilisable, à partirde ce middleware on a accés au corps de la requete
+  
+  /*requete post pour vendre un objet */
+  app.post('/api/SAUCES', (req, res, next) => {
+    delete req.body._id;
+    const newSauce = new sauceModel({
+      ...req.body
+    });
+    newSauce.save()
+      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
+
+  //requete get pour 1 élément du tableau
+  app.get('/api/SAUCES/:id', (req, res, next) => {
+    sauceModel.findOne({ _id: req.params.id })
+      .then(newSauce => res.status(200).json(newSauce))
+      .catch(error => res.status(404).json({ error }));
+  });
+
+  //app.get('/api/SAUCES/:_id', (req, res, next) => {
+    //sauce_model.findOne({ _id: req.params.id })
+    //.then(new_sauce => res.status(200).json(new_sauce))
+    //.catch(error => res.status(404).json({ error }));
+  //});
+
+  //requete get pour tous les éléments du tableau
+  app.use('/api/SAUCES', (req, res, next) => {
+    sauceModel.find()
+    .then(sauceModels => res.status(200).json(sauceModels)) //le tableau retourné par la base 
+    .catch(error => res.status(400).json({ error }));
+  });
+
+
+
+
 
 module.exports = app; //exportation de l'app créé
--- models/sauces.js
+++ models/sauces.js
@@ -1,20 +1,22 @@
-/*const mongoose = require('mongoose');
-
-const thingSchema = mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  imageUrl: { type: String, required: true },
-  userId: { type: String, required: true },
-  price: { type: Number, required: true },
-});
-
-module.exports = mongoose.model('Thing', thingSchema);*/
 
 const mongoose = require('mongoose');
 
-const sauceSchema = mongoose.Schema({
+const sauceSchema = mongoose.Schema({/*utilisation de la fonction Schema de mongoose auquel on passe un objet*/
+    //name: { type: String, required: true },
+    //manufacturer: { type: String, required: true },
+    userId: { type: String, required: true },
     name: { type: String, required: true },
     manufacturer: { type: String, required: true },
+    description: { type: String, required: true },
+    mainPepper: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    heat: { type: String, required: true },
+    likes: { type: Number, required: true },
+    dislikes: { type: Number, required: true },
+    usersLiked: { type: [String], required: true },
+    userDisliked: { type: [String], required: true },
+
 });
 
-module.exports = mongoose.model('Sauce', sauceSchema);
+//module.exports = mongoose.model('sauce_model', sauceSchema);
+module.exports = mongoose.model('sauceModel', sauceSchema);
